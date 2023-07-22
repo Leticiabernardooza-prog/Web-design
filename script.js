64330550830c99a955693a76f4c4fb11fcf24643
Leticiabernardooza-prog <55217048+Leticiabernardooza-prog@users.noreply.github.com>
2023-07-22 09:51:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/*rolagem suave*/
+  //botão voltar ao topo do site
+  $(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('a[href="#top"]').fadeIn();
+        } else {
+            $('a[href="#top"]').fadeOut();
+        }
+    });
+
+    $('a[href="#top"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
